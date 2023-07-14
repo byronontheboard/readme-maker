e6ef289f743de237d25ad64985c97cae7abaa298
byronallan <127366720+byronallan@users.noreply.github.com>
2023-07-13 20:04:37 -0700

    worked on tableOfContents and made my list of questions

---
+++ Develop/index.js
@@ -0,0 +1,89 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    { 
+        type: 'input',
+        name: 'firstName',
+        message: '',
+        
+    },
+    
+    { 
+        type: 'input',
+        name: 'lastName',
+        message: '',
+        
+    },
+    
+    { 
+        type: 'input',
+        name: 'projectTitle',
+        message: '',
+        
+    },
+    
+    { 
+        type: 'input',
+        name: 'projectDescription',
+        message: '',
+        
+    },
+    
+    { 
+        type: 'input',
+        name: 'projectInstall',
+        message: '',
+        
+    },
+    
+    { 
+        type: 'input',
+        name: 'projectUsage',
+        message: '',
+        
+    },
+    
+    { 
+        type: 'input',
+        name: 'projectContribution',
+        message: '',
+        
+    },
+    
+    { 
+        type: 'input',
+        name: 'projectTest',
+        message: '',
+        
+    },
+
+    {
+        type: 'input',
+        name: 'questions',
+        message: '',
+    },
+
+    { 
+        type: 'input',
+        name: 'userGitHub',
+        message: '',
+        
+    },
+
+    { 
+        type: 'input',
+        name: 'userEmail',
+        message: '',
+        
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
